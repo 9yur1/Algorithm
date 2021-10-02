ab56a9443245764b59454997068b2723937bb8da
9yur1 <kyuri0924@khu.ac.kr>
2021-10-03 02:26:25 +0900

    10.03

---
+++ Level1/10171.cpp
@@ -0,0 +1,17 @@
+/*
+고양이 출력하기!
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+
+*/
+
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "\\    /\\" << endl;
+	cout << " )  ( ')" << endl;
+	cout << "(  /  )" << endl;
+	cout << " \\(__)|" << endl;
+}
